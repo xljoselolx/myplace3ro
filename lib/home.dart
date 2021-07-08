4d659f362c4913a82cbf6ee7013893928555cc2d
xljoseolx <josegurka@gmail.com>
2021-07-08 10:47:58 -0400

    ¨3daparte¨

---
+++ lib/home.dart
@@ -0,0 +1,34 @@
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'description_place.dart';
+
+class MyHome extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final  descriptionPlace = Container(
+      margin: EdgeInsets.only(
+        top: 250,
+        left: 30,
+        right: 30
+      ),
+      child: DescriptionPlace("Uyuni", 3, "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."),
+    );
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+            "My Places"
+        ),
+      ),
+      body: Stack(
+        children: <Widget>
+        [
+          descriptionPlace
+        ],
+      ),
+    );
+  }
+  }
+
